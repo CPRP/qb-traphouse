a7668e39454039338d1a88377cb08d9799618390
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-14 14:53:10 -0400

    Added 3 more traphouses

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.MinZOffset = 40
 Config.TakeoverPrice = 5000
 Config.TrapHouses = {
-    [1] = {
+    [1] = { -- Postal 8219
         coords = {
             ["enter"] = vector3(-1202.21, -1308.48, 4.92),
             ["interaction"] = vector3(-1207.63, -1311.75, -32.65),
@@ -25,6 +25,126 @@ Config.TrapHouses = {
             },
             ["interaction"] = {
                 heading = 180,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            },
+            ["exit"] = {
+                heading = 180,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            }
+        },
+        keyholders = {},
+        pincode = 1234,
+        inventory = {},
+        opened = false,
+        takingover = false,
+        money = 0,
+    },
+    [2] = { -- Postal 1020
+        coords = {
+            ["enter"] = vector3(-39.08, 6420.5, 31.69),
+            ["interaction"] = vector3(-44.4, 6417.22, -5.88),
+        },
+        polyzoneBoxData = {
+            ["enter"] = {
+                heading = 44.14,
+                minZ = 29.0,
+                maxZ = 34.0,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 2.0,
+                created = false
+            },
+            ["interaction"] = {
+                heading = 185.49,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            },
+            ["exit"] = {
+                heading = 180,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            }
+        },
+        keyholders = {},
+        pincode = 1234,
+        inventory = {},
+        opened = false,
+        takingover = false,
+        money = 0,
+    },
+    [3] = { -- Postal 2009
+        coords = {
+            ["enter"] = vector3(1651.12, 4830.04, 42.03),
+            ["interaction"] = vector3(1645.76, 4826.76, 4.46),
+        },
+        polyzoneBoxData = {
+            ["enter"] = {
+                heading = 357.18,
+                minZ = 39.0,
+                maxZ = 45.0,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 2.0,
+                created = false
+            },
+            ["interaction"] = {
+                heading = 180.13,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            },
+            ["exit"] = {
+                heading = 180,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 1.0,
+                created = false
+            }
+        },
+        keyholders = {},
+        pincode = 1234,
+        inventory = {},
+        opened = false,
+        takingover = false,
+        money = 0,
+    },
+    [4] = { -- Postal 3031
+        coords = {
+            ["enter"] = vector3(438.48, 3570.54, 33.88),
+            ["interaction"] = vector3(433.15, 3567.26, -3.69),
+        },
+        polyzoneBoxData = {
+            ["enter"] = {
+                heading = 166.82,
+                minZ = 30.0,
+                maxZ = 36.0,
+                debug = false,
+                length = 1,
+                width = 1,
+                distance = 2.0,
+                created = false
+            },
+            ["interaction"] = {
+                heading = 181.46,
                 debug = false,
                 length = 1,
                 width = 1,
